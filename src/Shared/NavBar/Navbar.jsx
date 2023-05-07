e7230162ff78510209c17953605624cdda1e640c
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-05-07 07:23:51 +0100

    Creating new navbar using bootstrap framwork

--- src/Shared/NavBar/Navbar.jsx
+++ src/Shared/NavBar/Navbar.jsx
@@ -6,9 +6,9 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <div data-theme="luxury">
-            <div className="container-fluid">
+            <div className="container-fluid bg-background">
                 <nav className="navbar navbar-expand-md navbar-dark">
-                    <Link to="/" className="navbar-brand" href="#">
+                    <Link to="/" className="navbar-brand">
                         <img src={logo} width={150} />
                     </Link>
                     <button
@@ -23,24 +23,35 @@ const Navbar = () => {
                         <span className="navbar-toggler-icon" />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
+                        <ul className="navbar-nav mr-auto justify-content-center">
                             <li className="nav-item">
-                                <a className="nav-link " href="#">
-                                    Articles
+                                <a className="nav-link" href="#">
+                                    About us
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link " href="#">
-                                    Articles
+                                <a className="nav-link" href="#">
+                                    Our Services
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link " href="#">
+                                <a className="nav-link" href="#">
+                                    Our Portfolio
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Blog
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
                                     Contact
                                 </a>
                             </li>
                         </ul>
                     </div>
+
                 </nav>
             </div>
 
