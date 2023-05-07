655bc57daa7093f62339a128aaac9468c6205383
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-05-07 03:50:07 +0100

    Solving the problem of main caroussel in the responsive version

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,21 +6,22 @@ import Passion from '../Sections/Passion/Passion';
 import YourCreativity from '../Sections/YourCreativity/YourCreativity';
 import '../index.css';
 import WhatWeDo from '../Sections/WhatWeDo/WhatWeDo';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Home = () => {
     return (
         <div data-theme="luxury">
-            <Carousel showStatus={false} autoPlay={true} infiniteLoop={true} interval={2000}>
-                {/* Set autoPlay prop to true, infiniteLoop prop to true, and interval prop to the desired time in milliseconds */}
+            <Carousel showStatus={false} autoPlay={true} infiniteLoop={true} interval={2000} swipeable={false}>
                 <YourCreativity />
                 <OutOfReach />
                 <Passion />
             </Carousel>
-            <div style={{ margin: '-50px 0 0 0' }}> {/* Adjust the margin value as needed */}
+            <div> {/* Adjust the margin value as needed */}
                 <WhatWeDo />
             </div>
         </div>
     );
 };
+
 
 export default Home;
